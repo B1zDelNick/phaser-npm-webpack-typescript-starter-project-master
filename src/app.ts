810734f1932e5d1760a5674014660b2d1a146364
Andrei Vasilevich <a.vasilevich@gamesbannernet.com>
2017-12-25 10:02:18 +0700

    Nulled: 25.12.2017

--- src/app.ts
+++ src/app.ts
@@ -13,11 +13,6 @@ import * as Assets from './assets';
 import Start from './states/start';
 import {AssetMode, GameConfig, PublishMode, Sites} from './config/game.config';
 import {SaverTemplates} from './states/saver/enum.saver';
-import Comix from './states/comix';
-import Comix2 from './states/comix2';
-import Dress1 from './states/dress1';
-import Dress2 from './states/dress2';
-import Final from './states/final';
 
 class App extends Phaser.Game {
     constructor(config: Phaser.IGameConfig) {
@@ -26,11 +21,6 @@ class App extends Phaser.Game {
         this.state.add('Boot', Boot);
         this.state.add('Preloader', Preloader);
         this.state.add('Start', Start);
-        this.state.add('Comix', Comix);
-        this.state.add('Comix2', Comix2);
-        this.state.add('Dress1', Dress1);
-        this.state.add('Dress2', Dress2);
-        this.state.add('Final', Final);
 
         this.state.start('Boot');
     }
